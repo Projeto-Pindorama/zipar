7579e071b1206f70faa41d4f9ce0a453dec884dc
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-05-01 18:28:14 -0300

    feat: Implemented -t and -x options.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2025 Luiz Antônio Rangel (takusuman)
  *
- * SPDX-Licence-Identifier: MIT 
+ * SPDX-Licence-Identifier: MIT
  *
  */
 
@@ -11,23 +11,134 @@ package main
 
 import (
 	"archive/zip"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"pindorama.net.br/libcmon/zhip"
+	"rsc.io/getopt"
+	"strconv"
+)
+
+var (
+	fVerbose         bool
+	fTableOfContents bool
+	fExtract         bool
+	archive          string
+	areader          *zip.ReadCloser
 )
 
 func main() {
-	var file *zip.FileHeader
-	arc, err := zip.OpenReader(os.Args[1])
+	var err error
+
+	/* Options. */
+	flag.BoolVar(&fVerbose, "verbose", false,
+		"Enable verbose output.")
+	flag.BoolVar(&fTableOfContents, "toc", false,
+		"List the contents of the zipfile.")
+	flag.BoolVar(&fExtract, "extract", false,
+		"The named files are extracted from the zipfile.")
+	flag.StringVar(&archive, "file", "",
+		"Use the next argument as the name of the archive.")
+	getopt.Aliases(
+		"v", "verbose",
+		"t", "toc",
+		"x", "extract",
+		"f", "file",
+	)
+	getopt.Parse()
+
+	areader, err = zip.OpenReader(archive)
 	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"failed to open %s: %s\n",
+			archive, err)
+	}
+	defer areader.Close()
+
+	if fTableOfContents {
+		list_files(areader)
+	} else if fExtract {
+		for {
+			file := zhip.GetZipEntries(areader)
+			if file == nil {
+				break
+			}
+			extract_entry(file)
+		}
+	}
+}
+
+func list_files(arc *zip.ReadCloser) {
+	var file *zip.FileHeader
+
+	largest_file := len(strconv.FormatUint(
+		uint64(zhip.GetZipLargestEntry(arc)), 10))
+	for {
+		if file = zhip.GetZipEntries(arc); file == nil {
+			break
+		}
+		if fVerbose {
+			fmt.Printf("%*d %s:%02.0f%% %10s %s ",
+				largest_file,
+				file.UncompressedSize,
+				zhip.GetCompressionMethod(file),
+				zhip.GetCompressionRatio(file),
+				file.Mode().String(),
+				file.Modified.Format("2006-01-02 15:04:05"),
+			)
+		}
+		fmt.Println(file.Name)
+	}
+}
+
+func extract_entry(file *zip.FileHeader) {
+	var err error
+	var dest *os.File
+
+	if fVerbose {
+		fmt.Printf("x %s ", file.Name)
+		if file.FileInfo().IsDir() {
+			fmt.Println("directory")
+		} else {
+			fmt.Printf("%d bytes\n",
+				file.UncompressedSize)
+		}
+	}
+
+	if file.FileInfo().IsDir() {
+		err = os.MkdirAll(file.Name, file.Mode())
+	} else {
+		dest, err = os.Create(file.Name)
+		defer dest.Close()
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"failed to create %s: %s\n",
+			file.Name, err)
 		os.Exit(1)
 	}
-	defer arc.Close()
-
-	for notok := true; notok; notok = (file != nil) {
-		file = zhip.GetZipEntries(arc)	
- 		fmt.Printf("%#v\n", file)
+	if !file.FileInfo().IsDir() {
+		zfile, err := areader.File[(zhip.EntryNo[file.Name])].Open()
+		if err != nil {
+			fmt.Fprintf(os.Stderr,
+				"failed to open %s from %s: %s\n",
+				file.Name, archive, err)
+			os.Exit(1)
+		}
+		defer zfile.Close()
+		wbytes, err := io.Copy(dest, zfile)
+		if uint64(wbytes) != uint64(file.UncompressedSize) {
+			fmt.Fprintf(os.Stderr,
+				"failed to write %d bytes to the disk; wrote just %d: %s",
+				file.UncompressedSize, wbytes, err)
+			os.Exit(1)
+		}
+		err = os.Chmod(dest.Name(), file.Mode())
+		if err != nil {
+			fmt.Fprintf(os.Stderr,
+				"failed to restore permissions %04o for file %s: %s\n",
+				file.Mode(), dest.Name(), err)
+		}
 	}
-
-	// verbose: "x %s, %lld bytes, ""%lld%s\n"
 }
